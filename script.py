960b06f7d04d3f14f118d62e5e07268a53126bc5
Mariusz Krzyzopolski <krzyzopolski.mariusz@gmail.com>
2020-08-05 12:18:01 +0200

    overwrite and create db.implement days to next birthday with static leap day

--- script.py
+++ script.py
@@ -1,15 +1,16 @@
 import json
 from sqlalchemy import *
+from sqlalchemy_utils import *
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import *
 from datetime import datetime, timedelta,date
 
-engine = create_engine('sqlite:///users.db')
-Base = declarative_base()
 
-data = open("persons.json","r",encoding="utf8")
-f = json.loads(data.read())
+engine = create_engine('sqlite:///RandomUsers.db')
+if database_exists(engine.url): drop_database(engine.url) 
+create_database(engine.url)
+Base = declarative_base()
 
 class Users(Base):
 	__tablename__ = "users"
@@ -28,6 +29,21 @@ class Users(Base):
 	Cell = Column(Integer)
 	Password = Column(String)
 
+def calculateNextBirthday(birth):
+	today = date.today()
+	if(today.month == birth.month and today.day >= birth.day or today.month > birth.month):
+		nextBirthdayYear=today.year+1
+	else:
+		nextBirthdayYear=today.year
+	if(birth.day == 29 and birth.month == 2):
+		nextBirthdayYear=2024
+	nextBirthday=date(nextBirthdayYear, birth.month, birth.day)
+	return (nextBirthday-today).days
+
+
+data = open("persons.json","r",encoding="utf8")
+f = json.loads(data.read())
+
 
 Users.__table__.create(bind=engine, checkfirst=True)
 users = []
@@ -43,22 +59,22 @@ for i, result in enumerate(f['results']):
 	row['Username'] = result['login']['username']
 	row['Email'] = result['email']
 	row['Username'] = result['login']['username']
-	dob = datetime.strptime(result['dob']['date'],'%Y-%m-%dT%H:%M:%S.%fZ')#"%Y-%m-%dT%H:%M:%S%z")	
+	dob = datetime.strptime(result['dob']['date'],'%Y-%m-%dT%H:%M:%S.%fZ')
 	row['DOB'] = dob.date()
 	row['Age'] = result['dob']['age']
-	row['RDTB'] = dob.date()
+	row['RDTB'] = calculateNextBirthday(dob)
 	row['Cell'] = ''.join([i for i in result['cell'] if i.isdigit()])
 	row['Phone'] = ''.join([i for i in result['phone'] if i.isdigit()])
 	row['Password'] = result['login']['password']
-	
 	users.append(row)
+
 
 print(users[0])
 Session = sessionmaker(bind=engine)
 session = Session()
-
 for user in users:
-    row = Users(**user)
-    session.add(row)
+	row = Users(**user)
+	session.add(row)
 session.commit()
+session.close()
 # gender, name, location, email, login,dob,register,phone,cell,id,picture,nat
