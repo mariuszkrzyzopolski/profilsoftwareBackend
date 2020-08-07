fe05dbf28f64d02790869bd1b54047e72a0d9e7a
warhir <krzyzopolski.mariusz@gmail.com>
2020-08-07 11:33:12 +0200

    popular cities

--- script.py
+++ script.py
@@ -10,6 +10,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--start", help="start the program,import data to the database")
 parser.add_argument("-percentage-sex", help="percentage of men and women in database")
 parser.add_argument("-average-age", help="average age of people")
+parser.add_argument("-popular-city", help="most popular cities from database")
 args = parser.parse_args()
 Session = sessionmaker()
 conn = DataConn(Session)
@@ -51,4 +52,8 @@ elif args.average_age:
         result = conn.session.query(func.sum(Users.Age)).filter(Users.Gender.like('female')).scalar() / \
                  conn.session.query(func.count(Users.Age)).filter(Users.Gender.like('female')).scalar()
     print(f'Average age from database is {result}')
+elif args.popular_city:
+    result = conn.session.query(Users.City, func.count(Users.City)).group_by(Users.City).order_by(func.count(Users.City).desc()).limit(args.popular_city).all()
+    for city in result:
+        print(city)
 conn.session.close()
